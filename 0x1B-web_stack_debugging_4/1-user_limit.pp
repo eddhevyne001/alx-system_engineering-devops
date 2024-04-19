582817db1442d6875cf1e025b7a29e7d9e569f5b
eddhevyne001 <dhevyne001@gmail.com>
2024-04-19 14:39:47 +0000

    wbstack

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,2 +1,11 @@
-# A puppet script
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
